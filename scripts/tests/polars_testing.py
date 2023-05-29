89bcdae6f20a42a5a1e4cbe1aaef009e24daeaa6
Jake Zur <jakezur@umich.edu>
2023-05-29 00:44:57 -0400

    created div_factor, starting trend_factor.ipynb

--- scripts/tests/polars_testing.py
+++ scripts/tests/polars_testing.py
@@ -4,7 +4,6 @@ from dateutil.relativedelta import relativedelta
 
 from factorlib.factor import Factor
 from factorlib.factor_model import FactorModel
-from factorlib.transforms.expr_shortcuts import pct_change
 from factorlib.utils.system import get_data_dir, get_results_dir
 
 raw_data_dir = get_data_dir() / "raw"
@@ -16,25 +15,40 @@ stocks_data = pl.scan_csv(raw_data_dir / 'spy_data_daily.csv', try_parse_dates=T
 tickers = stocks_data.columns[1:]
 print("Universe of Tickers: ", len(tickers), " Total")
 stocks_data = stocks_data.select(pl.col('date_index'), pl.all().exclude('date_index').cast(float))
-returns_data = stocks_data.with_columns(pct_change(['date_index']))
+returns_data = stocks_data.with_columns(pl.col('date_index'),
+                                        pl.all().exclude('date_index').pct_change())
 
+print('Creating General Features...')
+# fff
 general_data_dir = get_data_dir() / 'general'
 fff_daily = (
     pl.scan_csv(general_data_dir / 'fff_daily.csv', try_parse_dates=True)
     .collect(streaming=True)
 )
+
 fff_factor = Factor(name='fff', data=fff_daily, current_interval='1d', general_factor=True, tickers=tickers)
 
+
+print('Creating Fundamental Features...')
+# fundamentals1_monthly
 fundamental_data_dir = get_data_dir() / 'fundamental'
 fundamentals1 = pl.scan_csv(fundamental_data_dir / 'fundamentals1_monthly.csv', try_parse_dates=True) \
     .collect(streaming=True)
 fundamentals1_factor = Factor(name='fundamentals1', data=fundamentals1,
                               current_interval='1mo', desired_interval='1d')
 
+# div_season
+div_season = pl.scan_csv(fundamental_data_dir / 'div_season.csv', try_parse_dates=True) \
+    .collect(streaming=True)
+div_season_factor = Factor(name='div_season', data=fundamentals1)
+
+
+print('Creating Model and Adding Factors...')
 model = FactorModel(tickers=tickers, interval='1d')
 
 model.add_factor(fff_factor)
 model.add_factor(fundamentals1_factor)
+model.add_factor(div_season_factor)
 
 stats = model.wfo(returns_data,
                   train_interval=relativedelta(years=5), anchored=False,  # interval parameters
